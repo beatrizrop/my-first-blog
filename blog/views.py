af9461916412083a5cc9fe540aa82ab05de90707
Beatriz Paegle <brop@cesar.school>
2019-07-26 10:58:33 -0300

    Changesss

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+"""from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 
@@ -6,4 +6,14 @@ from django.utils import timezone
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html', {'posts': posts})
+	return render(request, 'blog/post_list.html', {'posts': posts})"""
+
+
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    return render(request, 'blog/post_list.html', {'posts': posts})
